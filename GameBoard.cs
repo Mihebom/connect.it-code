bbf43dcc06ba9627e7285bd8e748c2db21ac5d4c
Mihebom <93613438+Mihebom@users.noreply.github.com>
2021-12-14 13:28:53 +0000

    refined gameboard with score updates etc.

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,16 +1,19 @@
+
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
 using TMPro;
 using UnityEngine.SceneManagement;
-using Photon.Pun;
+using Mirror;
+using UnityEngine.UI;
 
-public sealed class GameBoard : MonoBehaviour 
-{ 
-   
+
+
+public class GameBoard : NetworkBehaviour
+{
 
     public static GameBoard Instance { get; private set; }
 
@@ -22,39 +25,38 @@ public sealed class GameBoard : MonoBehaviour
 
     public int rowHeight => tiles.GetLength(1);
 
-    public TextMeshProUGUI timerDisplay;
+    public List<Tile> selection = new List<Tile>();
 
-    public int remainingSeconds = 30;
+    [SyncVar] private float TweenDuration = 0.25f;
 
-    public bool subtractTime = false;
+    [SerializeField] private AudioClip selectSound;
 
-    private readonly List<Tile> selection = new List<Tile>();
+    [SerializeField] private AudioSource _audioSource;
 
-    private const float TweenDuration = 0.25f;
+    public static Counter[] counters;
 
-    PhotonView view;
-
-
-    private void Awake()
+    public void Update()
     {
-        Instance = this;
-     
+        if (isServer && isClient)
+        {
+            serverUpdateBoardScore();
+        }
     }
 
+    #region Board Creation
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     void Start()
     {
+        counters = Resources.LoadAll<Counter>("FranceBoardCounters/");
 
-    
-        view = GetComponent<PhotonView>();
-       
-        timerDisplay.GetComponent<TextMeshProUGUI>().text = "00:" + remainingSeconds;
+        Instance = this;
 
-        
-        tiles = new Tile[rows.Max(row => row.tiles.Length), rows.Length];
+        tiles = new Tile[rows.Max(rowList => rowList.tiles.Length), rows.Length];
 
         for (var i = 0; i < rowHeight; i++)
         {
-            for(var j = 0; j < rowWidth; j++)
+            for (var j = 0; j < rowWidth; j++)
             {
 
                 var tile = rows[i].tiles[j];
@@ -62,130 +64,85 @@ public sealed class GameBoard : MonoBehaviour
                 tile.x = j;
                 tile.y = i;
 
-                tile.counter = CounterStore.counters[Random.Range(0, CounterStore.counters.Length)];
-                
+                tile.counter = counters[Random.Range(0, counters.Length)];
+
                 tiles[j, i] = tile;
             }
 
         }
 
-
-             
     }
 
-    void Update()
-    {
-      
+    #endregion
 
-
-            if(subtractTime == false && remainingSeconds > 0) { 
-        
-
-                StartCoroutine(timeSubtraction());
-
-            }
-
-            if (remainingSeconds <= 0)
-            {
-
-            SceneManager.LoadScene("demoOver");
-             
-        }
-
-
-
-
-
-    }
+    #region Board Intearction
 
     public async void Select(Tile tile)
     {
 
-      
-            if (!selection.Contains(tile)) selection.Add(tile);
+        if (!selection.Contains(tile)) selection.Add(tile);
 
-            if (selection.Count < 2) return;
+        if (selection.Count < 2) return;
 
-            Debug.Log($"Selected tiles at ({selection[0].x}, {selection[0].y}) and ({selection[1].x}, {selection[1].y}) ");
+        Debug.Log($"Selected tiles at ({selection[0].x}, {selection[0].y}) and ({selection[1].x}, {selection[1].y}) ");
 
+        await Swap(selection[0], selection[1]);
+
+        if (canPop())
+        {
+            pop();
+        }
+        else
+        {
             await Swap(selection[0], selection[1]);
+        }
 
-
-            if (canPop())
-            {
-
-                pop();
-
-            }
-            else
-            {
-
-                await Swap(selection[0], selection[1]);
-            }
-            selection.Clear();
-
-
-       
-
-
-
-
-
+        selection.Clear();
 
     }
 
     public async Task Swap(Tile tile1, Tile tile2)
     {
 
-      
-            var icon1 = tile1.icon;
-            var icon2 = tile2.icon;
-            var icon1Transform = icon1.transform;
-            var icon2Transform = icon2.transform;
-            var sequence = DOTween.Sequence();
+        var icon1 = tile1.icon;
+        var icon2 = tile2.icon;
+        var icon1Transform = icon1.transform;
+        var icon2Transform = icon2.transform;
+        var sequence = DOTween.Sequence();
 
-            sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
-                    .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
+                .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
 
-            await sequence.Play()
-                          .AsyncWaitForCompletion();
+        await sequence.Play()
+                      .AsyncWaitForCompletion();
 
-            icon1Transform.SetParent(tile2.transform);
-            icon2Transform.SetParent(tile1.transform);
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
 
-            tile1.icon = icon2;
-            tile2.icon = icon1;
+        tile1.icon = icon2;
+        tile2.icon = icon1;
 
-            var tile1Counter = tile1.counter;
-            tile1.counter = tile2.counter;
-            tile2.counter = tile1Counter;
-     
-
-        
+        var tile1Counter = tile1.counter;
+        tile1.counter = tile2.counter;
+        tile2.counter = tile1Counter;
 
     }
-
-
 
     private bool canPop()
     {
+        for (var y = 0; y < rowHeight; y++)
+            for (var x = 0; x < rowWidth; x++)
+                if (tiles[x, y].connectedTiles().Skip(1).Count() >= 2) return true;
 
-        for( var y = 0; y < rowHeight; y++)
-            for(var x = 0; x < rowWidth; x++)
-             if (tiles[x, y].connectedTiles().Skip(1).Count() >= 2) return true;
-
-               
-            
-
-        
-        
         return false;
     }
 
-    private async void pop()
+    public async void pop()
     {
+
+        
         for (var y = 0; y < rowHeight; y++)
-        {
+
             for (var x = 0; x < rowWidth; x++)
             {
 
@@ -197,35 +154,57 @@ public sealed class GameBoard : MonoBehaviour
 
                 var scaleDownSequence = DOTween.Sequence();
 
-                foreach(var matchedTile in matchedTiles) scaleDownSequence.Join(matchedTile.icon.transform.DOScale(Vector3.zero, TweenDuration));
+                foreach (var matchedTile in matchedTiles) scaleDownSequence.Join(matchedTile.icon.transform.DOScale(Vector3.zero, TweenDuration));
 
-                
+                _audioSource.PlayOneShot(selectSound);
+
+                if (isServer && isClient)
+                {
+                    if (matchedTiles.Count == 4)
+                    {
+                        ScoreSystem.instance.score += tile.counter.value * matchedTiles.Count * 2;
+
+                    }
+                    else if (matchedTiles.Count == 5)
+                    {
+                        ScoreSystem.instance.score += tile.counter.value * matchedTiles.Count * 3;
+                    }
+                    else
+                    {
+                        ScoreSystem.instance.score += tile.counter.value * matchedTiles.Count;
+                    }
+
+
+                }
+                else
+                {
+                    if (matchedTiles.Count == 4)
+                    {
+                        ScoreSystem.instance.opponentScore += tile.counter.value * matchedTiles.Count * 2;
+
+                    }
+                    else if (matchedTiles.Count == 5)
+                    {
+                        ScoreSystem.instance.opponentScore += tile.counter.value * matchedTiles.Count * 3;
+                    }
+                    else
+                    {
+                        ScoreSystem.instance.opponentScore += tile.counter.value * matchedTiles.Count;
+                    }
+
+                }
 
                 await scaleDownSequence.Play()
-                                        .AsyncWaitForCompletion();
-
-
-                if (matchedTiles.Count == 4)
-                {
-                    ScoreSystem.instance.score += tile.counter.value * matchedTiles.Count * 2;
-
-                } else if(matchedTiles.Count == 5)
-                {
-                    ScoreSystem.instance.score += tile.counter.value * matchedTiles.Count * 3;
-                } else
-                {
-                    ScoreSystem.instance.score += tile.counter.value * matchedTiles.Count;
-                }
+                                   .AsyncWaitForCompletion();
 
                 var upScaleSequence = DOTween.Sequence();
 
-                foreach(var matchedTile in matchedTiles)
+                foreach (var matchedTile in matchedTiles)
                 {
 
-                    matchedTile.counter = CounterStore.counters[Random.Range(0, CounterStore.counters.Length)];
+                    matchedTile.counter = counters[Random.Range(0, counters.Length)];
 
                     upScaleSequence.Join(matchedTile.icon.transform.DOScale(Vector3.one, TweenDuration));
-
 
                 }
 
@@ -235,33 +214,30 @@ public sealed class GameBoard : MonoBehaviour
                 x = 0;
                 y = 0;
 
-
             }
-
-        }
     }
 
-   
-    IEnumerator<WaitForSeconds> timeSubtraction()
+    #endregion
+
+    #region Score Update
+
+    [Server]
+    public void serverUpdateBoardScore()
     {
-       
-        subtractTime = true;
-        yield return new WaitForSeconds(1);
-        remainingSeconds -= 1;
-        if (remainingSeconds < 10)
-        {
-            timerDisplay.GetComponent<TextMeshProUGUI>().text = "00:0" + remainingSeconds;
-
-        }
-        else
-        {
-            timerDisplay.GetComponent<TextMeshProUGUI>().text = "00:" + remainingSeconds;
-        }
-        subtractTime = false;
-
+        rpcUpdateBoardScore();
     }
 
+    [ClientRpc]
+    public void rpcUpdateBoardScore()
+    {
+        ScoreSystem.instance.opponentScoreText.SetText($"Score = {ScoreSystem.instance._opponentScore}");
+        //Updates server score on client
+        ScoreSystem.instance.scoreText.SetText($"Score = {ScoreSystem.instance._score}");
+    }
 
-
+    #endregion
 
 }
+
+
+
