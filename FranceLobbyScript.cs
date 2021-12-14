b28f08e4a866aae6f644824aeefcad3c930de290
Mihebom <93613438+Mihebom@users.noreply.github.com>
2021-12-14 13:34:06 +0000

    lobby script used by both boards

---
+++ FranceLobbyScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class FranceLobbyScript : MonoBehaviour
+{
+
+    public NetworkManager networkManager;
+
+    public void Host()
+    {
+
+        networkManager.StartHost();
+    }
+
+    public void joinIP(string joiningIP)
+    {
+
+        networkManager.networkAddress = joiningIP;
+
+    }
+
+    public void Join()
+    {
+        networkManager.StartClient();
+    }
+
+
+}
