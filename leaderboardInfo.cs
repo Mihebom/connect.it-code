be0a896defa4c5ed7dc30ae46eb9c7ddb2f9fe30
Mihebom <93613438+Mihebom@users.noreply.github.com>
2022-02-16 22:39:04 +0000

    creates individual leaderboard entries

---
+++ leaderboardInfo.cs
@@ -0,0 +1,17 @@
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class leaderboardInfo : MonoBehaviour
+{
+
+    public Text displaynameTxt;
+    public Text scoreTxt;
+  
+    
+}
