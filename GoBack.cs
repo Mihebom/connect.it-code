923f9e20ff7eadc23667d064258c92ae374b241b
Mihebom <93613438+Mihebom@users.noreply.github.com>
2022-03-30 00:02:22 +0100

    Update: new scene change - back to boardlist

--- GoBack.cs
+++ GoBack.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Photon.Pun;
 
 
-public class goBack : MonoBehaviour
+
+public class GoBack : MonoBehaviour
 {
     public void backToMain()
     {
         SceneManager.LoadScene("MainMenu");
    
 
+    }
+
+ 
+
+    public void backToSelector()
+    {
+
+        SceneManager.LoadScene("BoardList");
     }
 }
