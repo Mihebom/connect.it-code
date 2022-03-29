1d45b75fe826764a206797c88a31d4ef1b2e9bd6
Mihebom <93613438+Mihebom@users.noreply.github.com>
2022-03-29 23:51:56 +0100

    Update: now creates all country board counters

--- Counter.cs
+++ Counter.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Connect.it/FranceBoardCounters")]
+[CreateAssetMenu(menuName = "Connect.it/CountryCounters")]
+
 public sealed class Counter : ScriptableObject
 {
-
+    
+    
     public int value;
 
     public Sprite sprite;
