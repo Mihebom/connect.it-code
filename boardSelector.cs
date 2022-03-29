dba45ab410243c8afbb57a311c76e608eb69d82c
Mihebom <93613438+Mihebom@users.noreply.github.com>
2022-03-29 23:59:51 +0100

    Update: added matchmaking scene changes

--- boardSelector.cs
+++ boardSelector.cs
@@ -3,33 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class boardSelector : MonoBehaviour
+public class BoardSelector : MonoBehaviour
 {
     public void franceBoard()
     {
         SceneManager.LoadScene("FranceBoard");
-
-
     }
 
     public void chinaBoard()
     {
         SceneManager.LoadScene("ChinaBoard");
-
-
     }
 
     public void chinaLobby()
     {
         SceneManager.LoadScene("ChinaLobby");
-
-
     }
   
     public void franceLobby()
     {
         SceneManager.LoadScene("FranceLobby");
+    }
 
+    public void nigeriaLobby()
+    {
+        SceneManager.LoadScene("NigeriaLobby");
+    }
 
+    public void nigeriaBoard()
+    {
+        SceneManager.LoadScene("NigeriaBoard");
+    }
+
+    public void pakistanLobby()
+    {
+        SceneManager.LoadScene("PakistanLobby");
+    }
+
+    public void franceMatch()
+    {
+        SceneManager.LoadScene("FranceMatchMaking");
+    }
+
+    public void chinaMatch()
+    {
+        SceneManager.LoadScene("ChinaMatchMaking");
+    }
+
+    public void pakistanMatch()
+    {
+        SceneManager.LoadScene("PakistanMatchMaking");
+    }
+
+    public void nigeriaMatch()
+    {
+        SceneManager.LoadScene("NigeriaMatchMaking");
     }
 }
+
