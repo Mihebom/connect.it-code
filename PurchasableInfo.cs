8007fcadcd68a79a7e475c97551e8fd1da60b898
Mihebom <93613438+Mihebom@users.noreply.github.com>
2022-03-30 00:05:25 +0100

    Add files via upload

---
+++ PurchasableInfo.cs
@@ -0,0 +1,18 @@
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PurchasableInfo : MonoBehaviour
+{
+    public TextMeshProUGUI itemdisplayName;
+    public TextMeshProUGUI itemCost;
+    public Image itemIMG;
+    public Button button;
+
+}
